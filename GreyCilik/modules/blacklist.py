7d140c738e4f6b1634b2b502b1213cd6224b24ba
myjhon <112201041+azzurezxv@users.noreply.github.com>
2022-09-18 14:01:02 +0700

    Update blacklist.py

--- GreyCilik/modules/blacklist.py
+++ GreyCilik/modules/blacklist.py
@@ -6,18 +6,18 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import mention_html
 
-import GreyCilik.modules.sql.blacklist_sql as sql
-from GreyCilik import dispatcher, LOGGER
-from GreyCilik.modules.disable import DisableAbleCommandHandler
-from GreyCilik.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from GreyCilik.modules.helper_funcs.extraction import extract_text
-from GreyCilik.modules.helper_funcs.misc import split_message
-from GreyCilik.modules.log_channel import loggable
-from GreyCilik.modules.warns import warn
-from GreyCilik.modules.helper_funcs.string_handling import extract_time
-from GreyCilik.modules.connection import connected
-from GreyCilik.modules.sql.approve_sql import is_approved
-from GreyCilik.modules.helper_funcs.alternate import send_message, typing_action
+import Afffaantuhhhh.modules.sql.blacklist_sql as sql
+from Afffaantuhhhh import dispatcher, LOGGER
+from Afffaantuhhhh.modules.disable import DisableAbleCommandHandler
+from Afffaantuhhhh.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from Afffaantuhhhh.modules.helper_funcs.extraction import extract_text
+from Afffaantuhhhh.modules.helper_funcs.misc import split_message
+from Afffaantuhhhh.modules.log_channel import loggable
+from Afffaantuhhhh.modules.warns import warn
+from Afffaantuhhhh.modules.helper_funcs.string_handling import extract_time
+from Afffaantuhhhh.modules.connection import connected
+from Afffaantuhhhh.modules.sql.approve_sql import is_approved
+from Afffaantuhhhh.modules.helper_funcs.alternate import send_message, typing_action
 
 BLACKLIST_GROUP = 11
 
