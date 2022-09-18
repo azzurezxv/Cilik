5c8b76471ed575ec023775168f5a4e9c7e34f628
myjhon <112201041+azzurezxv@users.noreply.github.com>
2022-09-18 12:46:16 +0700

    Update config.py

--- GreyCilik/config.py
+++ GreyCilik/config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/GreyCilik/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/Afffaantuhhhh/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -18,8 +18,8 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 1784606556  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "greyyvbss"
-    SUPPORT_CHAT = "CilikSupport"  # Your own group for support, do not add the @
+    OWNER_USERNAME = "Afffaantuhhhh"
+    SUPPORT_CHAT = "azzurezxv"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001692951846
     )  # Prints any new group the bot is added to, prints just the name and ID.
