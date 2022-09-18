6f09feeaf9c2bfc5ab5354adea56cfc41c5da067
myjhon <112201041+azzurezxv@users.noreply.github.com>
2022-09-18 12:49:53 +0700

    Update mongo.py

--- GreyCilik/mongo.py
+++ GreyCilik/mongo.py
@@ -3,13 +3,13 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from GreyCilik import MONGO_DB_URI
-from GreyCilik.confing import get_int_key, get_str_key
+from Afffaantuhhhh import MONGO_DB_URI
+from Afffaantuhhhh.confing import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "GreyCilik"
+MONGO_DB = "Afffaantuhhhh"
 
 
 client = MongoClient()
