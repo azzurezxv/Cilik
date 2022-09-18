6ec63225d8e73a6c549868ab887c0dc41578515e
myjhon <112201041+azzurezxv@users.noreply.github.com>
2022-09-18 13:54:02 +0700

    Update anime.py

--- GreyCilik/modules/anime.py
+++ GreyCilik/modules/anime.py
@@ -5,8 +5,8 @@ import textwrap
 import bs4
 import jikanpy
 import requests
-from GreyCilik import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
-from GreyCilik.modules.disable import DisableAbleCommandHandler
+from Afffaantuhhhh import DEV_USERS, OWNER_ID, DRAGONS, dispatcher
+from Afffaantuhhhh.modules.disable import DisableAbleCommandHandler
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, ParseMode,
                       Update)
 from telegram.ext import CallbackContext, CallbackQueryHandler
